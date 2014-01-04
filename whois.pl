49978b919f10d09618abcfec23669ec4c73297de
CodeThat <CodeThat@users.noreply.github.com>
2014-01-04 11:47:26 -0600

    Update whois.pl

--- whois.pl
+++ whois.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+#TODO: Add much more options for tracing within one program.
+
 print ("Enter the whois search term:\n");
 chomp($domain = <STDIN>);
 system whois -h whois.arin.net; 
