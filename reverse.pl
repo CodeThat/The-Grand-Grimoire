a5d1ef0aa2040b38e3f53c476b6fd6937115674b
CodeThat <CodeThat@users.noreply.github.com>
2014-01-04 11:55:52 -0600

    Create reverse.pl

---
+++ reverse.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+#Don't even really remember wtf I was doing here...
+
+@tag=split(/-/,@ARGV[0]);
+@ip1=split(/\ ./,@tag[0]);
+@ip2=split(/\ ./,@tag[1]);
+
+for($a=@ip1[0]; $a<1+@ip2[0]; $a++) {
+        for ($b=@ip1[1]; $b<1+@ip2[1]; $b++) {
+                for ($c=@ip1[2]; $c<1+@ip2[2]; $c++) {
+                        for ($d=@ip1[3]; $d<1+@ip2[3]; $d++) {
+                                $mwe = 'host $a.$b.$c.$d';
+chop ($mwe);
+chop ($mwe);
+
+if (substr($mwe, 0, 4) ne "Host") {
+        $mwe =~ s/.*pointer //g;
+print "$a.$b.$c.$d = $mwe\ n";
+                        }
+                }
+        }
+}
+}
+
