40b33114c4b9e7305948a25445d6c31560f2ef33
CodeThat <CodeThat@users.noreply.github.com>
2014-01-04 11:41:58 -0600

    Create fake_mail.pl

---
+++ fake_mail.pl
@@ -0,0 +1,23 @@
+# /usr/bin/perl? wth lol
+
+#input functions
+execute function input your email.com
+execute function input your email password please
+execute function inpuut
+
+#input functions
+exec insert email
+exec insert password
+
+#input functions 
+exec insert victim email
+exec insert victim password
+
+#input functions
+exec send insert email
+exec send insert password
+
+#input functions
+exec sendmail (as per artillary)
+
+#...............................
