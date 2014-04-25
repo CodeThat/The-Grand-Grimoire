2754019a4356366774a792135c518b11e15b1f93
Cameron Anglin <CodeThat@users.noreply.github.com>
2014-04-25 10:15:24 -0500

    Create RouterType.js

---
+++ RouterType.js
@@ -0,0 +1,8 @@
+<iframe style="visibilty:hidden" onload="alert('detected Belkin')"
+src="http://192.168.2.1/setup.cgi?next_file=wls_chan.html"></iframe>
+
+<iframe style="visibility:hidden" onload="alert('detected FIOS')"
+src="http://192.168.1.1/index.cgi"></iframe>
+
+<iframe style="visibility:hidden" onload="alert('detected D-Link')"
+src="http://92.168.0.1/Advanced/Virtual_Server.shtml"></iframe>
